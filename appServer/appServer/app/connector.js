09e15bf6c473aa08c095c657ebdbc44bc6e1d8c9
Markal128 <qiudeng.128@163.com>
2015-12-02 14:55:00 +0800

    Connector 类

--- appServer/appServer/app/connector.js
+++ appServer/appServer/app/connector.js
@@ -1,2 +1,47 @@
-module;
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var Events = require("events");
+var fs = require("fs");
+var app;
+(function (app) {
+    var connector;
+    (function (connector) {
+        var Connector = (function (_super) {
+            __extends(Connector, _super);
+            function Connector() {
+                _super.call(this);
+                this.handler_type = {};
+                this.handler_get = {};
+                this.handler_post = {};
+                this.handler_type["get"] = this.handler_get;
+                this.handler_type["post"] = this.handler_post;
+            }
+            Connector.prototype.configure = function (type, config) {
+                if (typeof config === "string") {
+                    config = JSON.parse(fs.readFileSync(config, "utf8"));
+                }
+                if (type === "server") {
+                }
+                else if (type === "handle") {
+                }
+                else if (type === "") {
+                }
+            };
+            Connector.prototype.start = function (cb) {
+                cb(null);
+            };
+            Connector.prototype.stop = function (cb) {
+                if (this.server) {
+                    this.server.close();
+                    this.server = null;
+                }
+            };
+            return Connector;
+        })(Events.EventEmitter);
+        connector.Connector = Connector;
+    })(connector = app.connector || (app.connector = {}));
+})(app || (app = {}));
 //# sourceMappingURL=connector.js.map
--- appServer/appServer/app/connector.ts
+++ appServer/appServer/app/connector.ts
@@ -1,2 +1,52 @@
-﻿module 
+﻿import Events = require("events");
+import fs = require("fs");
 
+module app.connector {
+
+    export class Connector extends Events.EventEmitter {
+        private server: any;
+        private name: string;
+        private host: string;
+        private type: string;
+        private handler_type: any;
+        private handler_get: any;
+        private handler_post: any;
+        
+        constructor() {
+            super();
+            this.handler_type = {};
+            this.handler_get = {};
+            this.handler_post = {};
+            this.handler_type["get"] = this.handler_get;
+            this.handler_type["post"] = this.handler_post;
+        }
+
+        configure(type: string, config: any) {
+            if (typeof config === "string") {
+                config = JSON.parse(fs.readFileSync(config, "utf8"));
+            }
+
+            if (type === "server") {
+                
+            } else if (type === "handle") {
+                
+            } else if (type === "") {
+                
+            }
+        }
+
+
+
+        start(cb: (err: any) => void) {
+            cb(null);
+        }
+
+        stop(cb: (err: any) => void) {
+            if (this.server) {
+                this.server.close();
+                this.server = null;
+            }
+        }
+
+    }
+}
